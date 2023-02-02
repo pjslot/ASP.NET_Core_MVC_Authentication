9043e358bf7e9d8ffb41968eeab0cb467c81d063
pjslot <pjslot@gmail.com>
2023-02-02 18:44:03 +0300

    release 1.0

--- ASP.NET_Core_MVC_Authentication/Models/UserViewModels.cs
+++ ASP.NET_Core_MVC_Authentication/Models/UserViewModels.cs
@@ -12,11 +12,11 @@ namespace ASP.NET_Core_MVC_Authentication.Models
 	}
 	public class RoleМodificationМodel
 	{
-		[Required]
+	//	[Required]
 		public string RoleName { get; set; }
 		public string RoleId { get; set; }
-		public string[] IdsToAdd { get; set; }
-		public string[] IdsToDelete { get; set; }
+		public string[]? IdsToAdd { get; set; }
+		public string[]? IdsToDelete { get; set; }
 	}
 
 }
